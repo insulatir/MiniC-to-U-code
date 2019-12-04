36d87e6a3572cc840b649b464d52604c96bb7195
insulator <s42415466@gmail.com>
2019-12-04 14:47:59 +0900

    test.uco 파일 쓰기

--- src/listener/main/UCodeGenListener.java
+++ src/listener/main/UCodeGenListener.java
@@ -100,17 +100,17 @@ public class UCodeGenListener extends MiniCBaseListener implements ParseTreeList
 		// 트리에 최종 문장 추가
 		newTexts.put(ctx, program);
 		
-//		// 'Test.j' 파일
-//		File file = new File("Test.j");
-//		
-//		try {
-//			FileWriter fw = new FileWriter(file);
-//			// 'Test.j' 파일에 program 쓰기
-//			fw.write(newTexts.get(ctx));
-//			fw.close();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
+		// 'test.uco' 파일
+		File file = new File("test.uco");
+		
+		try {
+			FileWriter fw = new FileWriter(file);
+			// 'test.uco' 파일에 program 쓰기
+			fw.write(newTexts.get(ctx));
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 		// program 출력
 		System.out.println(newTexts.get(ctx));
