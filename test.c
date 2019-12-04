0a9a59048627fd324fbce936c0adb33b6aa05ef3
insulator <s42415466@gmail.com>
2019-12-04 14:48:08 +0900

    배열 추가

--- test.c
+++ test.c
@@ -1,19 +1,24 @@
-int max = 100;
+int MAX = 10;
 
-int func(int t) {
-   
-   int sum = 0;
-   if((t==max)){
-     while(sum <= 100){
-         ++sum;
-      }
-   }
-   return sum;
+void main(void)
+{
+	int i;
+	int a[10];
+	int j;
+	i = j = 0;
+	while (i < MAX) {
+		a[i] = j;
+		a[i] = sub(i, a);
+		j = j + a[i];
+		++i;
+	}
+	_print(j);
+	return;
 }
 
-void main () {
-   int t = 100;  
-   _print(func(t));
-   
-   return;
+int sub(int i, int a[]) {
+	int j;
+	
+	j = j + a[i];
+	return j;
 }
